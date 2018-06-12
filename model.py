fe3b3c71891c7fd86ac6e99fff1b8f9b622ce182
Rahul Patil <rahul.p3@ugamsolutions.com>
2018-06-12 17:17:41 +0530

    modelv3 added, 91.9% acc, no overfitting for 30 epochs

--- model.py
+++ model.py
@@ -92,7 +92,7 @@ def conv_model(no_of_classes):
     model.add(MaxPooling2D(pool_size = (2, 2), strides = 2))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'same'))  # second convolution unit
+    model.add(Conv2D(64, (3, 3), activation = 'relu', padding = 'same'))  # third convolution unit
     model.add(MaxPooling2D(pool_size = (2, 2), strides = 2))
     model.add(Dropout(0.25))
 
